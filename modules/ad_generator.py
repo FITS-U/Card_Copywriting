2dfc55a5e2cf53989140af5a14c1a82e261bd8cd
Ubuntu <ubuntu@ip-172-31-32-189.ap-northeast-3.compute.internal>
2024-12-10 06:34:09 +0000

    updated
    Former-commit-id: 8c34798e6baa9f1609d2039a37d1da3ac33b1b15

--- modules/ad_generator.py
+++ modules/ad_generator.py
@@ -23,13 +23,11 @@ def generate_advertising_copy(card_name, benefits):
         # 프롬프트 템플릿
         prompt_template = """
         당신은 뛰어난 광고 카피라이터입니다. 카드 혜택을 바탕으로 카드의 매력을 전달하는 창의적이고 감동적인 카피를 제작하세요.
-        - 카드의 혜택을 강조하고, 실용적이거나 감성적인 접근으로 사용자의 관심을 끌어야 합니다.
         - 다양한 스타일로 접근하세요: 예를 들어, 한 문구는 즐겁고 감동적인 톤으로, 다른 문구는 실용적이고 간결한 톤으로 작성해 주세요.
         - 이모티콘으로 생동감을 더하고 직관적으로 전달되도록 하세요.
         - 카드 이름도 적절한 위치에 넣어주세요. 
         - 두 줄로 분량 제한하고, 각 줄은 엔터 처리로 분리
         - 광고 문구가 아닌 쓸데없는 말이 있으면 절대로 안됩니다.
-        - 광고 문구에 숫자도 절대 있으면 안됩니다.
         - '**' 같은 특수문자는 절대 있으면 안됩니다.
         카드 정보:
         - 이름: {card_name}
@@ -61,8 +59,8 @@ def generate_ads_for_user(filtered_recommendations, card_info):
         benefits = row['mainCtgNameListStr']
 
         # 카드 이름 가져오기
-        card_name = card_info[card_info['card_id'] == card_id]['card_name']
-        print(f"card_info : {card_name}")
+        card_name = card_info.loc[card_info['card_id'] == card_id, 'card_name'].values[0]
+        image_url = card_info.loc[card_info['card_id'] == card_id, 'image_url'].values[0]
         # 광고 문구 생성
         ad_copy = generate_advertising_copy(card_name, benefits)
 
@@ -71,7 +69,8 @@ def generate_ads_for_user(filtered_recommendations, card_info):
             "card_id": card_id,
             "card_name": card_name,
             "benefits": benefits,
-            "adCopy": ad_copy
+            "adCopy": ad_copy,
+            "image_url":image_url
         })
 
     return pd.DataFrame(ad_results)
@@ -93,7 +92,8 @@ def process_ad_results(ad_results):
         processed_results.append({
             "card_id": ad['card_id'],
             "adCopy1": ad_copy_1,
-            "adCopy2": ad_copy_2
+            "adCopy2": ad_copy_2,
+            "image_url":ad['image_url']
         })
 
     return pd.DataFrame(processed_results)
