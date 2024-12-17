1dfa55f65022d7c8f77a19900d5a9b859121887c
zefo38 <110655450+zefo38@users.noreply.github.com>
2024-12-17 17:45:57 +0900

    Update

--- modules/contents.py
+++ modules/contents.py
@@ -1,19 +1,44 @@
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from scipy.sparse import csr_matrix
+from konlpy.tag import Mecab
+import matplotlib.pyplot as plt
+import seaborn as sns
+import logging
+from sklearn.feature_extraction.text import TfidfVectorizer
+from matplotlib.colors import LinearSegmentedColormap
+logging.getLogger("matplotlib").setLevel(logging.WARNING)
+def tokenized_vectorize_cosine(card_ctg_list):
+    mecab = Mecab()
 
-# 카드 혜택 데이터를 벡터화
-def vectorize_card_data(card_ctg_list):
-    vectorizer = CountVectorizer()
-    ctg_matrix = vectorizer.fit_transform(card_ctg_list['mainCtgNameListStr'])
+    # 정규 표현식 수행
+    card_ctg_list['ctg_name_list'] = card_ctg_list['ctg_name_list'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","", regex=True)
 
-    return ctg_matrix, vectorizer.get_feature_names_out()
+    # 불용어 정의
+    stopwords = ['언제나','지원']
+    
+    texts = []
+    for idx,text in enumerate(card_ctg_list['ctg_name_list']):
+        # 형태소 분석
+        tokens = mecab.morphs(text)
+        print(f"원본 텍스트 [{idx}]: {text}")
+        print(f"형태소 분석 결과: {tokens}")
+        # 불용어 제거
+        tokens = [word for word in tokens if word not in stopwords]
+        print(f"불용어 제거 결과: {tokens}")
+        print("-" * 50)  # 구분선 추가
+        texts.append(" ".join(tokens))
 
 
+    tfidf_vectorizer = TfidfVectorizer()
+    embeddings = tfidf_vectorizer.fit_transform(texts)
+    cosine_sim_matrix = cosine_similarity(embeddings)
+    print(cosine_sim_matrix)
+    similarity_df = pd.DataFrame(
+                        cosine_sim_matrix, 
+                        index=card_ctg_list['card_id'], 
+                        columns=card_ctg_list['card_id']
+                        )
 
-def calculate_card_similarity(ctg_matrix, card_ctg_list):
-    # 희소 행렬로 계산
-    cate_sim = cosine_similarity(csr_matrix(ctg_matrix))
-    similarity_df = pd.DataFrame(cate_sim, index=card_ctg_list['card_id'], columns=card_ctg_list['card_id'])
     return similarity_df
+
+
